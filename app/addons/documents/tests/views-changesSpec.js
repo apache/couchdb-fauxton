755eb7a7532f23eb22871ccb36559c6f18b6b941
Garren Smith <garren.smith@gmail.com>
2014-08-12 16:55:28 +0200

    Fix changesSpec

--- app/addons/documents/tests/views-changesSpec.js
+++ app/addons/documents/tests/views-changesSpec.js
@@ -28,7 +28,7 @@ define([
 
     handlerSpy = sinon.spy(Views.Changes.prototype, 'toggleJson');
 
-    beforeEach(function () {
+    beforeEach(function (done) {
       var database = new Databases.Model({id: 'bla'});
       database.buildChanges({descending: 'true', limit: '100', include_docs: 'true'} );
       filteredView = new Views.Changes({
@@ -39,7 +39,7 @@ define([
         model: model
       });
       viewSandbox = new ViewSandbox();
-      viewSandbox.renderView(view);
+      viewSandbox.renderView(view, done);
     });
 
     afterEach(function () {
