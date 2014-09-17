e89d3ac75f599b4999a2c8a5e191483e211bdf43
Garren Smith <garren.smith@gmail.com>
2014-09-17 11:34:47 +0200

    Fix api bar in documents page

--- app/addons/documents/views.js
+++ app/addons/documents/views.js
@@ -98,12 +98,16 @@ function(app, FauxtonAPI, Components, Documents, Databases, Views, QueryOptions,
       }));*/
 
       //Moved the apibar view into the components file so you can include it in your views
-      this.apiBar = this.insertView("#header-api-bar", new Components.ApiBar({}));
+      this.apiBar = this.insertView("#header-api-bar", new Components.ApiBar({
+        endpoint: this.apiEndpoints[0],
+        documentation: this.apiEndpoints[1]
+      }));
     },
 
     updateApiUrl: function(api){
       //this will update the api bar when the route changes
       //you can find the method that updates it in components.js Components.ApiBar()
+      this.apiEndpoints = api;
       this.apiBar && this.apiBar.update(api);
     },
 
