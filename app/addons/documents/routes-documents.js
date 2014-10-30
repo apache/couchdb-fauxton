eac686e443a7a926dd11c74464ddb73f784448f5
Robert Kowalski <robertkowalski@apache.org>
2014-10-30 12:28:03 +0100

    Just hide the rightHeader on changes
    Fixes COUCHDB-2419

--- app/addons/documents/routes-documents.js
+++ app/addons/documents/routes-documents.js
@@ -107,6 +107,10 @@ function(app, FauxtonAPI, Documents, Changes, Index, DocEditor, Databases, Resou
         }
       });
 
+      this.rightHeader = this.setView("#right-header", new Documents.Views.RightAllDocsHeader({
+         database: this.database
+      }));
+
       this.leftheader = this.setView("#breadcrumbs", new Components.LeftHeader({
         crumbs: crumbs.allDocs(this.database),
         dropdownMenu: this.setUpDropdown()
@@ -219,10 +223,6 @@ function(app, FauxtonAPI, Documents, Changes, Index, DocEditor, Databases, Resou
         return;
       }
 
-      this.rightHeader = this.setView("#right-header", new Documents.Views.RightAllDocsHeader({
-        database: this.database
-      }));
-
       this.leftheader.updateCrumbs(crumbs.allDocs(this.database));
       this.database.buildAllDocs(docParams);
 
@@ -298,9 +298,6 @@ function(app, FauxtonAPI, Documents, Changes, Index, DocEditor, Databases, Resou
        return [this.indexedDocs.urlRef("apiurl", urlParams), "docs"];
       };
 
-      this.rightHeader = this.setView("#right-header", new Documents.Views.RightAllDocsHeader({
-        database: this.database
-      }));
       this.rightHeader.showQueryOptions();
       this.rightHeader.resetQueryOptions({
         queryParams: urlParams,
@@ -455,9 +452,6 @@ function(app, FauxtonAPI, Documents, Changes, Index, DocEditor, Databases, Resou
 
     changes: function () {
       var docParams = app.getParams();
-
-      this.rightHeader && this.rightHeader.remove();
-
       this.database.buildChanges(docParams);
 
       this.changesView = this.setView("#dashboard-lower-content", new Changes.Changes({
@@ -477,6 +471,7 @@ function(app, FauxtonAPI, Documents, Changes, Index, DocEditor, Databases, Resou
 
       this.sidebar.setSelectedTab('changes');
       this.leftheader.updateCrumbs(crumbs.changes(this.database));
+      this.rightHeader.hideQueryOptions();
 
       this.apiUrl = function () {
         return [this.database.url("changes-apiurl"), this.database.documentation()];
