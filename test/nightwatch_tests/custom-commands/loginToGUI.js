236a961772db8bdba66aadbaadd5fa4f3b53f35b
Michelle Phung <michellephung@gmail.com>
2014-11-21 13:12:44 -0500

    Replace .execute() for .click() behavior in Nightwatch tests

--- test/nightwatch_tests/custom-commands/loginToGUI.js
+++ test/nightwatch_tests/custom-commands/loginToGUI.js
@@ -10,7 +10,8 @@ exports.command = function () {
     .waitForElementPresent('#username', 8000, false)
     .setValue('#username', ['tester'])
     .setValue('#password', ['testerpass', client.Keys.ENTER])
-    .execute('$("#global-notifications button.close").click();')
+    .waitForElementVisible('#global-notifications', 8000, false)
+    .click('#global-notifications button.close')
     .waitForElementPresent('#jump-to-db', 8000, false);
       
   return this;
