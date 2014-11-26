eaf9dc61651245112b5ed8dfd7b275ab6c2db270
Benjamin Keen <ben.keen@gmail.com>
2014-11-26 08:43:05 -0800

    Layout templates should limit header width
    This was added to fix minor flashing issues because the #dashboard
    element wasn't wide enough for its children elements and they were
    being repositioned during repaints. It ensures the visible width is
    correct so the API Url doesn't disappear offscreen as well.

---
+++ app/addons/databases/tests/nightwatch/confirmAPIUrlTrayOpens.js
@@ -0,0 +1,25 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+module.exports = {
+  'Confirm API Url tray opens' : function (client) {
+    var waitTime = 10000,
+        baseUrl = client.globals.baseUrl;
+
+    client
+      .loginToGUI()
+      .url(baseUrl + '/#/_all_docs')
+      .click('.api-url-btn')
+      .waitForElementVisible('.api-navbar', waitTime, false)
+      .end();
+  }
+};
