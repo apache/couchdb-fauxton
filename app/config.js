1da49a39422b1f31233a97007e228f9a86b52af1
Benjamin Keen <ben.keen@gmail.com>
2015-01-13 12:55:15 -0800

    Moved momentjs to js/libs folder
    I placed the moment.js lib in the wrong location in an earlier
    commit. This puts it into the libs folder to be with its friends.

--- app/config.js
+++ app/config.js
@@ -33,7 +33,7 @@ require.config({
     "cloudant.pagingcollection": "../assets/js/plugins/cloudant.pagingcollection",
     "velocity": "../assets/js/plugins/velocity",
     "velocity.ui": "../assets/js/plugins/velocity.ui",
-    moment: '../assets/js/plugins/moment'
+    moment: '../assets/js/libs/moment'
   },
 
   baseUrl: '/',
