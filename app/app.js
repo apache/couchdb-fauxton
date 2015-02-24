7f118dfc0f9b150063af399c5c76c09e0ef3a3ac
Robert Kowalski <robertkowalski@apache.org>
2015-02-24 15:24:39 +0100

    Polyfill window.location.origin
    Old browser do not support `window.location.origin` so we are
    polyfilling it.

--- app/app.js
+++ app/app.js
@@ -43,6 +43,16 @@ function(app, $, _, Backbone, Bootstrap, Helpers, constants, Utils, FauxtonAPI, 
     };
   }
 
+  // make sure we have location.origin
+  if (_.isUndefined(window.location.origin)) {
+    var port = '';
+    if (window.location.port) {
+      port = ':' + window.location.port;
+    }
+    window.location.origin = window.location.protocol + '//' +
+      window.location.hostname + port;
+  }
+
   // Provide a global location to place configuration settings and module
   // creation also mix in Backbone.Events
   _.extend(app, {
