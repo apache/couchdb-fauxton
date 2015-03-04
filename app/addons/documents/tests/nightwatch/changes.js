204fa54914ecd2f3a498bf26187542f4c6d3d811
Robert Kowalski <robertkowalski@apache.org>
2015-03-04 13:29:17 +0100

    remove select-all button on changes view
    closes COUCHDB-2576

---
+++ app/addons/documents/tests/nightwatch/changes.js
@@ -0,0 +1,28 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+module.exports = {
+  'Does not display the Select-all-button': function (client) {
+    var waitTime = 10000,
+        newDatabaseName = client.globals.testDatabaseName,
+        baseUrl = client.globals.test_settings.launch_url;
+
+    client
+      .loginToGUI()
+      .url(baseUrl + '/#/database/' + newDatabaseName + '/_all_docs')
+      .waitForElementPresent('.control-toggle-alternative-header', waitTime, false)
+      .click('#changes')
+      .waitForElementVisible('.changes-view', waitTime, false)
+      .assert.elementNotPresent('.control-toggle-alternative-header')
+    .end();
+  }
+};
