5c276e8c0a6885b5a5acb31ff68fbf81139232a2
Ben Keen <ben.keen@gmail.com>
2015-03-23 16:07:47 -0700

    Design doc names truncated in sidebar
    Long design doc names displayed in the sidebar of the Docs page would
    overlap with the rightmost ("+") icon. This truncates them with an
    ellipsis and adds a tooltip (just a title attr) to provide a means to
    let user see the full design doc name.

--- app/addons/databases/tests/nightwatch/checkDatabaseTooltip.js
+++ app/addons/databases/tests/nightwatch/checkDatabaseTooltip.js
@@ -33,8 +33,8 @@ module.exports = {
       .clickWhenVisible('label[for="checkbox-' + newDocumentName + '"]', waitTime, false)
       .clickWhenVisible('.control-delete', waitTime, false)
       .acceptAlert()
-      .waitForElementVisible('.alert.alert-info', waitTime, false)
-      .click('#nav-links a[href="#/_all_dbs"]')
+      .waitForElementVisible('#global-notifications .alert.alert-info', waitTime, false)
+      .clickWhenVisible('#nav-links a[href="#/_all_dbs"]')
 
       // now let's look at the actual UI to confirm the tooltip appears
       .waitForElementPresent('.js-db-graveyard', waitTime, false)
