ccd37aeea460ae7ebacd7a9f8e893f268cabe4de
Garren Smith <garren.smith@gmail.com>
2015-04-27 13:29:54 +0100

    Fauxton Server for use with NPM
    This allows Fauxton to be installed via npm so that we can release newer
    versions of Fauxton for users to try out.
    
      $ npm install fauxton -g
      $ fauxton
    
    or see `fauxton --help` for options

---
+++ index.js
@@ -0,0 +1,89 @@
+var path = require("path");
+var http = require("http");
+var httpProxy = require('http-proxy');
+var send = require('send');
+var urlLib = require('url');
+var _ = require('lodash');
+var dist_dir = __dirname + '/dist/release/';
+
+module.exports = function (options) {
+  // Options
+  var setContentSecurityPolicy = options.contentSecurityPolicy;
+  var port = options.port;
+  var proxyUrl = options.couchdb;
+
+  function sendFile (req, res, filePath) {
+    return send(req, filePath)
+    .on('error', function (err) {
+      if (err.status === 404) {
+        console.log('Could not locate', filePath);
+      } else {
+        console.log('ERROR', filePath, err);
+      }
+
+      res.setHeader("Content-Type", "text/javascript");
+      res.statusCode = 404;
+      res.end(JSON.stringify({error: err.message}));
+    })
+    .pipe(res);
+  }
+
+  // create proxy to couch for all couch requests
+  var proxy = httpProxy.createServer({
+    target: proxyUrl
+  });
+
+  http.createServer(function (req, res) {
+    var url = req.url,
+    accept = req.headers.accept.split(',');
+
+    if (setContentSecurityPolicy) {
+      var headerValue = "default-src 'self'; img-src 'self' data:; font-src 'self'; " +
+      "script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline';";
+      res.setHeader('Content-Security-Policy', headerValue);
+    }
+
+    if (url === '/' && accept[0] !== 'application/json') {
+      // serve main index file from here
+      return sendFile(req, res, path.join(dist_dir, 'index.html'));
+    } else if (accept[0] !== 'application/json') {
+      return sendFile(req, res, path.join(dist_dir, url.split(/\?v=|\?noCache/)[0]));
+    }
+
+    // This sets the Host header in the proxy so that one can use external
+    // CouchDB instances and not have the Host set to 'localhost'
+    var urlObj = urlLib.parse(req.url);
+    req.headers.host = urlObj.host;
+
+    proxy.web(req, res);
+  }).listen(port);
+
+  proxy.on('error', function (e) {
+    // don't explode on cancelled requests
+  });
+
+  //Remove Secure on the cookie if the proxy is communicating to a CouchDB instance
+  // via https.
+  proxy.on('proxyRes', function (proxyRes, req, res) {
+    if (proxyRes.headers['set-cookie']) {
+      proxyRes.headers['set-cookie'][0] = proxyRes.headers["set-cookie"][0].replace('Secure', '');
+    }
+  });
+
+  var logo = [
+    [""],
+    [" ______                        _                   "],
+    ["|  ____|                      | |                  "],
+    ["| |__    __ _   _   _  __  __ | |_    ___    _ __  "],
+    ["|  __|  / _` | | | | | \\ \\/ / | __|  / _ \\  | '_ \\ "],
+    ["| |    | (_| | | |_| |  >  <  | |_  | (_) | | | | |"],
+    ["|_|     \\__,_|  \\__,_| /_/\\_\\  \\__|  \\___/  |_| |_|"],
+    [""]
+  ];
+
+  _.each(logo, function (line) {
+    console.log(line.toString());
+  });
+
+  console.log('Listening on ' + port);
+};
