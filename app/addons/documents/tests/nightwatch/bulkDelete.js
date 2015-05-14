13b760b4187a235f260907696405f22268b4ed2e
Ben Keen <ben.keen@gmail.com>
2015-05-14 15:22:02 -0700

    Improving robustness of bulkDelete.js
    This test has been known to fail. This slows it down a bit to ensure
    elements are present and visible before they are clicked. I was able
    to see this test fail locally; this has fixed it for me.

--- app/addons/documents/tests/nightwatch/bulkDelete.js
+++ app/addons/documents/tests/nightwatch/bulkDelete.js
@@ -14,10 +14,10 @@ module.exports = {
 
   'Bulk deletes': function (client) {
     var waitTime = client.globals.maxWaitTime,
-        newDatabaseName = client.globals.testDatabaseName,
-        newDocumentName1 = 'bulktest1',
-        newDocumentName2 = 'bulktest2',
-        baseUrl = client.globals.test_settings.launch_url;
+      newDatabaseName = client.globals.testDatabaseName,
+      newDocumentName1 = 'bulktest1',
+      newDocumentName2 = 'bulktest2',
+      baseUrl = client.globals.test_settings.launch_url;
 
     client
       .loginToGUI()
@@ -25,18 +25,18 @@ module.exports = {
       .createDocument(newDocumentName2, newDatabaseName)
       .url(baseUrl + '/#/database/' + newDatabaseName + '/_all_docs')
       .waitForElementPresent('.control-toggle-alternative-header', waitTime, false)
-      .click('.control-toggle-alternative-header')
+      .clickWhenVisible('.control-toggle-alternative-header')
       .waitForElementPresent('.control-select-all', waitTime, false)
-      .click('.control-select-all')
-      .click('.control-delete')
+      .clickWhenVisible('.control-select-all', waitTime, false)
+      .clickWhenVisible('.control-delete', waitTime, false)
       .acceptAlert()
       .waitForElementVisible('#global-notifications .alert.alert-info', waitTime, false)
       .waitForElementNotPresent('[data-id="' + newDocumentName1 + '"]', waitTime, false)
       .getText('body', function (result) {
         var data = result.value,
-            isPresentFirstDoc = data.indexOf(newDocumentName1) !== -1,
-            isPresentSecondDoc = data.indexOf(newDocumentName2) !== -1,
-            bothMissing = !isPresentFirstDoc && !isPresentSecondDoc;
+          isPresentFirstDoc = data.indexOf(newDocumentName1) !== -1,
+          isPresentSecondDoc = data.indexOf(newDocumentName2) !== -1,
+          bothMissing = !isPresentFirstDoc && !isPresentSecondDoc;
 
         this.verify.ok(bothMissing,
           'Checking if documents were deleted');
@@ -46,23 +46,23 @@ module.exports = {
 
   'Select all works after changing the page': function (client) {
     var waitTime = client.globals.maxWaitTime,
-        newDatabaseName = client.globals.testDatabaseName,
-        baseUrl = client.globals.test_settings.launch_url;
+      newDatabaseName = client.globals.testDatabaseName,
+      baseUrl = client.globals.test_settings.launch_url;
 
     client
       .loginToGUI()
       .createManyDocuments(25, newDatabaseName)
       .url(baseUrl + '/#/database/' + newDatabaseName + '/_all_docs')
       .waitForElementPresent('.control-toggle-alternative-header', waitTime, false)
-      .click('.control-toggle-alternative-header')
+      .clickWhenVisible('.control-toggle-alternative-header')
       .waitForElementPresent('.control-select-all', waitTime, false)
-      .click('.control-select-all')
-      .waitForElementVisible('#next', waitTime, false)
-      .click('#next')
+      .clickWhenVisible('.control-select-all', waitTime, false)
+      .waitForElementPresent('#next', waitTime, false)
+      .clickWhenVisible('#next', waitTime, false)
       .waitForElementVisible('[data-id="27"]', waitTime, false)
-      .click('#previous')
+      .waitForElementPresent('#previous', waitTime, false)
+      .clickWhenVisible('#previous', waitTime, false)
       .waitForElementPresent('.control-select-all.js-headerbar-togglebutton-selected', waitTime, false)
       .end();
   }
-
 };
