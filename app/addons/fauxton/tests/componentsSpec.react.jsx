d27af31141670ae10dd1fdf74ada06b87092ef12
Robert Kowalski <robertkowalski@apache.org>
2015-05-20 10:35:56 +0200

    tests: cleanup more tests
     - make react 13 compatible
     - do not patch jquery
     - fix async race conditions
     - do not couple tightly to implementation (event handlers)

--- app/addons/fauxton/tests/componentsSpec.react.jsx
+++ app/addons/fauxton/tests/componentsSpec.react.jsx
@@ -13,7 +13,7 @@ define([
   'api',
   'addons/fauxton/components.react',
   'testUtils',
-  "react"
+  'react'
 ], function (FauxtonAPI, Views, utils, React) {
 
   var assert = utils.assert;
@@ -21,106 +21,59 @@ define([
 
   describe('Tray', function () {
 
-    var container, anotherContainer, trayEl, done, old$;
-    // trace registrations
-    var handlersOn, handlersOff, velocities;
+    var container, trayEl, done;
 
     beforeEach(function () {
-      handlersOn = [];
-      handlersOff = [];
-      velocities = [];
-      done = sinon.spy();
-      // simulate $ to test registration
-      old$ = $;
-      $ = function (what) {
-        this.on = function (id, handler) {
-          handlersOn.push([what, id]);
-        };
-        this.off = function (id) {
-          handlersOff.push([what, id]);
-        };
-        this.velocity = function (trans, speed, callback) {
-          velocities.push([what, trans]);
-          callback();
-        };
-        return this;
-      };
       container = document.createElement('div');
       // when we want to control the diff, we have to render directly
-      trayEl = React.renderComponent(React.createElement(Views.Tray, {className: "traytest"}), container);
+      trayEl = React.render(<Views.Tray className="traytest" />, container);
+      done = sinon.spy();
     });
 
     afterEach(function () {
-      $ = old$;
       React.unmountComponentAtNode(container);
-      if (anotherContainer) {
-        React.unmountComponentAtNode(anotherContainer);
-      }
     });
 
     it('renders trayid and custom classes', function () {
-      assert(trayEl.getDOMNode().getAttribute("class").indexOf("traytest") >= 0);
-    });
-
-    it('registers handler with body', function () {
-      assert.equal(1, handlersOn.length);
-      assert.equal("body", handlersOn[0][0]);
-      assert.equal("click.Tray-", handlersOn[0][1].substring(0, "click.Tray-".length));
-      assert.equal(0, handlersOff.length);
-      // also a 2nd time
-      anotherContainer = document.createElement('div');
-      React.renderComponent(React.createElement(Views.Tray, {className: "traytest"}), anotherContainer);
-      assert.equal(2, handlersOn.length);
-      assert.equal("body", handlersOn[1][0]);
-      assert.equal("click.Tray-", handlersOn[1][1].substring(0, "click.Tray-".length));
-      assert.equal(0, handlersOff.length);
-      // (we have different IDs)
-      assert(handlersOn[0][1] != handlersOn[1][1]);
-      // and we also unregister properly
-      var unmountSuccess = React.unmountComponentAtNode(anotherContainer);
-      assert(unmountSuccess);
-      assert.equal(1, handlersOff.length);
-      assert.equal("body", handlersOff[0][0]);
-      assert.equal("click.Tray-", handlersOff[0][1].substring(0, "click.Tray-".length));
-      // (the ID of the 2nd element)
-      assert(handlersOn[1][1] == handlersOff[0][1]);
+      assert(trayEl.getDOMNode().getAttribute('class').indexOf('traytest') >= 0);
     });
 
     it('is initially closed', function () {
-      assert.equal("none", trayEl.getDOMNode().style.display);
+      assert.equal('none', trayEl.getDOMNode().style.display);
     });
 
     it('shows when requested', function () {
-      trayEl.setVisible(true);
-      assert.equal(1, velocities.length);
-      assert.equal("block", trayEl.getDOMNode().style.display);
+      trayEl.setVisible(true, function () {
+        assert.equal('block', trayEl.getDOMNode().style.display);
+      });
     });
 
     it('hides when requested', function () {
-      trayEl.show();
-      trayEl.setVisible(false);
-      assert.equal(2, velocities.length);
-      assert.equal("none", trayEl.getDOMNode().style.display);
+      trayEl.show(function () {
+        trayEl.setVisible(false, function () {
+          assert.equal('none', trayEl.getDOMNode().style.display);
+        });
+      });
     });
 
     it('does nothing when already hidden', function () {
-      trayEl.setVisible(false);
-      assert.equal(0, velocities.length);
+      trayEl.setVisible(false, function () {
+        throw new Error('should do nothing');
+      });
     });
 
     it('toggles open with callback', function () {
-      trayEl.toggle(done);
-      assert.ok(done.calledOnce);
-      assert.equal(1, velocities.length);
-      assert.equal("block", trayEl.getDOMNode().style.display);
+      trayEl.toggle(function () {
+        assert.equal('block', trayEl.getDOMNode().style.display);
+      });
     });
 
     it('toggles close again with callback', function () {
-      trayEl.show();
-      trayEl.toggle(done);
-      assert.ok(done.calledOnce);
-      assert.equal(2, velocities.length);
-      assert.equal("none", trayEl.getDOMNode().style.display);
+      trayEl.show(function () {
+        trayEl.toggle(function () {
+          assert.equal('none', trayEl.getDOMNode().style.display);
+        });
+      });
     });
 
   });
@@ -142,8 +95,12 @@ define([
       React.unmountComponentAtNode(container);
     });
 
-    it("renders 20-wise pages per default", function () {
-      var pageEl = React.renderComponent(React.createElement(Views.Pagination, {page: 3, total: 55, urlPrefix: "?prefix=", urlSuffix: "&suffix=88"}), container);
+    it('renders 20-wise pages per default', function () {
+      var pageEl = React.render(
+        <Views.Pagination page={3} total={55} urlPrefix="?prefix=" urlSuffix="&suffix=88" />,
+        container
+      );
+
       var lis = pageEl.getDOMNode().getElementsByTagName("li");
       assert.equal(1 + 3 + 1, lis.length);
       assert(nvl(lis[0].getAttribute("class")).indexOf("disabled") < 0);
@@ -156,13 +113,20 @@ define([
     });
 
     it("can overwrite collection size", function () {
-      var pageEl = React.renderComponent(React.createElement(Views.Pagination, {perPage: 10, page: 3, total: 55}), container);
+      var pageEl = React.render(
+        <Views.Pagination perPage={10} page={3} total={55} urlPrefix="?prefix=" urlSuffix="&suffix=88" />,
+        container
+      );
+
       var lis = pageEl.getDOMNode().getElementsByTagName("li");
       assert.equal(1 + 6 + 1, lis.length);
     });
 
     it("handles large collections properly - beginning", function () {
-      var pageEl = React.renderComponent(React.createElement(Views.Pagination, {page: 3, total: 600}), container);
+      var pageEl = React.render(
+        <Views.Pagination page={3} total={600} />,
+        container
+      );
       var lis = pageEl.getDOMNode().getElementsByTagName("li");
       assert.equal(1 + 10 + 1, lis.length);
       assert(nvl(lis[3].getAttribute("class")).indexOf("active") >= 0);
@@ -172,7 +136,11 @@ define([
     });
 
     it("handles large collections properly - middle", function () {
-      var pageEl = React.renderComponent(React.createElement(Views.Pagination, {page: 10, total: 600}), container);
+      var pageEl = React.render(
+        <Views.Pagination page={10} total={600} />,
+        container
+      );
+
       var lis = pageEl.getDOMNode().getElementsByTagName("li");
       assert.equal(1 + 10 + 1, lis.length);
       assert(nvl(lis[6].getAttribute("class")).indexOf("active") >= 0);
@@ -182,7 +150,11 @@ define([
     });
 
     it("handles large collections properly - end", function () {
-      var pageEl = React.renderComponent(React.createElement(Views.Pagination, {page: 29, total: 600}), container);
+      var pageEl = React.render(
+        <Views.Pagination page={29} total={600} />,
+        container
+      );
+
       var lis = pageEl.getDOMNode().getElementsByTagName("li");
       assert.equal(1 + 10 + 1, lis.length);
       assert(nvl(lis[9].getAttribute("class")).indexOf("active") >= 0);
