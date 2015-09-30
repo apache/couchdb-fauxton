62038259c53e2595e45095891eec72ccc61cadd1
Ben Keen <ben.keen@gmail.com>
2015-09-30 14:54:04 -0700

    Add a test to confirm test exists
    A pet peeve with running a single mocha test on the command line,
    like `grunt file --file=./a/b/c.js` is that (a) it spends several
    seconds compiling, copying etc. then (b) just ends. It doesn't
    notify you that the file doesn't exist.
    
    This little PR adds a test that runs up front: if the file you
    entered doesn't exist, it lets you know about it and stops the
    process immediately.

--- Gruntfile.js
+++ Gruntfile.js
@@ -364,6 +364,7 @@ module.exports = function (grunt) {
       }
     },
     gen_initialize: templateSettings,
+    checkTestExists: templateSettings,
 
     mkcouchdb: couch_config,
     rmcouchdb: couch_config,
@@ -540,7 +541,7 @@ module.exports = function (grunt) {
    */
   // clean out previous build artifacts and lint
   grunt.registerTask('lint', ['clean', 'jshint']);
-  grunt.registerTask('test', ['clean:release', 'dependencies', 'jsx', 'jshint', 'shell:stylecheck', 'gen_initialize:development', 'test_inline']);
+  grunt.registerTask('test', ['checkTestExists', 'clean:release', 'dependencies', 'jsx', 'jshint', 'shell:stylecheck', 'gen_initialize:development', 'test_inline']);
 
   // lighter weight test task for use inside dev/watch
   grunt.registerTask('test_inline', ['mochaSetup', 'jst', 'concat:test_config_js', 'shell:phantomjs']);
