4c25810d085a91505bfc05c2aafbf9f15a55de5f
Robert Kowalski <robertkowalski@apache.org>
2016-01-19 18:31:55 +0100

    always update api url
    also update api url when routes are in the same route object.
    
    steps to reproduce:
    
    1. click on api bar in _find area for mango
    2. click edit
    3. click api bar button on _index page
    4. url should point to _index

---
+++ app/addons/fauxton/tests/nightwatch/updatesUrlsSameRouteobject.js
@@ -0,0 +1,33 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+module.exports = {
+  'it updates the API url even for routes in the same routeobject' : function (client) {
+    var waitTime = client.globals.maxWaitTime,
+        newDatabaseName = client.globals.testDatabaseName,
+        baseUrl = client.globals.test_settings.launch_url;
+
+    client
+      .populateDatabase(newDatabaseName)
+      .loginToGUI()
+      .url(baseUrl + '/#/database/' + newDatabaseName + '/_find')
+      .clickWhenVisible('.control-toggle-api-url')
+      .waitForElementVisible('.text-field-to-copy', waitTime, false)
+      .assert.value('.text-field-to-copy', 'http://localhost:8000/' + newDatabaseName + '/_find')
+      .clickWhenVisible('.edit-link')
+      .waitForElementVisible('.prettyprint', waitTime, false)
+      .clickWhenVisible('.control-toggle-api-url')
+      .waitForElementVisible('.text-field-to-copy', waitTime, false)
+      .assert.value('.text-field-to-copy', 'http://localhost:8000/' + newDatabaseName + '/_index')
+      .end();
+  }
+};
