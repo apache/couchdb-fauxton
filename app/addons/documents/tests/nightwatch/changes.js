f2fdb268bdc7a28dc1cdf1fccdb6f12e2ac7a872
Robert Kowalski <robertkowalski@apache.org>
2016-01-26 13:55:30 +0100

    fix tests

--- app/addons/documents/tests/nightwatch/changes.js
+++ app/addons/documents/tests/nightwatch/changes.js
@@ -20,10 +20,10 @@ module.exports = {
     client
       .loginToGUI()
       .url(baseUrl + '/#/database/' + newDatabaseName + '/_all_docs')
-      .waitForElementPresent('.header-toggle-button', waitTime, false)
+      .waitForElementPresent('.two-sides-toggle-button', waitTime, false)
       .clickWhenVisible('#changes')
       .waitForElementPresent('.js-changes-view', waitTime, false)
-      .assert.elementNotPresent('.header-toggle-button')
+      .assert.elementNotPresent('.two-sides-toggle-button')
       .end();
   },
 
