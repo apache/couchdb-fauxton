91bb6e44f145353c87b66810c19d6728be73a265
Robert Kowalski <robertkowalski@apache.org>
2016-09-05 15:11:42 +0200

    add license headers
    PR: #768
    PR-URL: https://github.com/apache/couchdb-fauxton/pull/768
    Reviewed-By: Robert Newson <rnewson@apache.org>

--- bin/install-docker-travis.sh
+++ bin/install-docker-travis.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" > /etc/apt/sources.list.d/docker.list'
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-get update
