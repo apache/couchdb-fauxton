3940f717eaf1495688405543646d280921c228cd
Tim Pinington <timpinington@gmail.com>
2016-11-24 10:13:42 +0000

    Fix broken test

--- app/addons/auth/base.js
+++ app/addons/auth/base.js
@@ -24,7 +24,7 @@ Auth.initialize = function () {
   FauxtonAPI.addHeaderLink({
     id: 'auth',
     title: 'Login',
-    href: '#login',
+    href: '#/login',
     icon: 'fonticon-user',
     bottomNav: true
   });
