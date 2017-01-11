09cf05e9c5a9efae8e9cd880ec1239657478652e
Garren Smith <garren.smith@gmail.com>
2017-01-11 17:25:44 +0200

    clean up app.js

--- app/app.js
+++ app/app.js
@@ -9,9 +9,8 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
+import "jquery";
 import app from "./initialize";
-import $ from "jquery";
-window.$ = $;
 import _ from "lodash";
 import "bootstrap";
 import Helpers from "./helpers";
