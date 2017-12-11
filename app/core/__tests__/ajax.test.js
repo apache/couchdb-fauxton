4a03a10eab1863afa46a75c5a0d2486e65edae55
Garren Smith <garren.smith@gmail.com>
2017-12-11 18:19:36 +0200

    Add new ajax api
    A small api on top of fetch along with an observable so that we can view
    all fetch requests. This is the basis to remove all $.ajax requests and
    use fetch instead.

---
+++ app/core/__tests__/ajax.test.js
@@ -0,0 +1,144 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+import fetchMock from 'fetch-mock';
+import {
+  json,
+  fetchObserver,
+  get,
+  put,
+  post,
+  deleteRequest
+} from '../ajax';
+import testUtils from "../../../test/mocha/testUtils";
+const assert = testUtils.assert;
+
+describe('Fauxton Ajax', () => {
+  let unsubscribe;
+
+  afterEach(() => {
+    if (unsubscribe) {
+      unsubscribe.unsubscribe();
+      unsubscribe = null;
+    }
+  });
+
+  it('should observe multiple requests', (done) => {
+    fetchMock.getOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    fetchMock.getOnce('/testing2', {
+      status: 201,
+      body: {
+        ok: true
+      }
+    });
+
+    let count = 0;
+    unsubscribe = fetchObserver.subscribe({
+      next () {
+        count++;
+        if (count === 2) {
+          done();
+        }
+      }
+    });
+
+    json("/testing");
+    json("/testing2");
+  });
+
+  it('Observer should be filterable', (done) => {
+    fetchMock.getOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    fetchMock.getOnce('/testing-again', {
+      status: 400,
+      body: {
+        hello: 'ok'
+      }
+    });
+
+    unsubscribe = fetchObserver.filter(resp => resp.status === 400).subscribe({
+      next (resp) {
+        done();
+        assert.deepEqual(resp.status, 400);
+      }
+    });
+
+    json("/testing");
+    json("/testing-again");
+  });
+
+  it('can do a GET', () => {
+    fetchMock.getOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    return get('/testing')
+    .then(resp =>{
+      assert.ok(resp.ok);
+    });
+  });
+
+  it('can do a PUT', () => {
+    fetchMock.putOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    return put('/testing')
+    .then(resp =>{
+      assert.ok(resp.ok);
+    });
+  });
+
+  it('can do a POST', () => {
+    fetchMock.postOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    return post('/testing')
+    .then(resp =>{
+      assert.ok(resp.ok);
+    });
+  });
+
+  it('can do a DELETE', () => {
+    fetchMock.deleteOnce('/testing', {
+      status: 200,
+      body: {
+        ok: true
+      }
+    });
+
+    return deleteRequest('/testing')
+    .then(resp =>{
+      assert.ok(resp.ok);
+    });
+  });
+});
