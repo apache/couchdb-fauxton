b1ee7cdac66cf6a4141e6491bfccd00edf076149
Antonio Maranhao <30349380+Antonio-Maranhao@users.noreply.github.com>
2018-03-19 14:19:19 -0400

    Remove all uses of JQuery from the verifyinstall addon (#1060)
    * Remove all uses of JQuery in the verifyinstall addon

--- app/addons/verifyinstall/resources.js
+++ app/addons/verifyinstall/resources.js
@@ -12,21 +12,22 @@
 
 import app from "../../app";
 import FauxtonAPI from "../../core/api";
+import { get, post } from "../../core/ajax";
 import Databases from "../databases/resources";
 import Documents from "../documents/resources";
-var Verifyinstall = FauxtonAPI.addon();
+const Verifyinstall = FauxtonAPI.addon();
 
-var db = new Databases.Model({
+const db = new Databases.Model({
   id: 'verifytestdb',
   name: 'verifytestdb'
 });
 
-var dbReplicate = new Databases.Model({
+const dbReplicate = new Databases.Model({
   id: 'verifytestdb_replicate',
   name: 'verifytestdb_replicate'
 });
 
-var doc, viewDoc;
+let doc, viewDoc;
 
 Verifyinstall.testProcess = {
   saveDoc: function () {
@@ -51,55 +52,56 @@ Verifyinstall.testProcess = {
   },
 
   setupDB: function (db) {
-    var deferred = FauxtonAPI.Deferred();
-    db.fetch()
-      .then(function () {
+    const promise = new FauxtonAPI.Promise((resolve, reject) => {
+      db.fetch().then(() => {
         return db.destroy();
-      }, function () {
-        deferred.resolve();
-      })
-      .then(function () {
-        deferred.resolve();
-      }, function (xhr, error, reason) {
-        if (reason === 'Unauthorized') {
-          deferred.reject(xhr, error, reason);
-        }
+      }, () => {
+        resolve();
+      }).then(() => {
+        resolve();
+      }, (xhr, error, reason) => {
+        reject(new Error(reason));
       });
-
-    return deferred;
+    });
+    return promise;
   },
 
   setup: function () {
-    return FauxtonAPI.when([
+    return FauxtonAPI.Promise.all([
       this.setupDB(db),
       this.setupDB(dbReplicate)
     ]);
   },
 
   testView: function () {
-    var deferred = FauxtonAPI.Deferred();
-    var promise = $.get(viewDoc.url() + '/_view/testview');
-
-    promise.then(function (resp) {
-      resp = _.isString(resp) ? JSON.parse(resp) : resp;
-      var row = resp.rows[0];
-      if (row.value === 6) {
-        return deferred.resolve();
+    let resolve, reject;
+    const promise = new FauxtonAPI.Promise(function(res, rej) {
+      resolve = res;
+      reject = rej;
+    });
+    const getPromise = get(viewDoc.url() + '/_view/testview').then(res => {
+      if (res.error) {
+        throw new Error(res.reason || res.error);
       }
-      var reason = {
-        reason: 'Values expect 6, got ' + row.value
-      };
+      return res;
+    });
 
-      deferred.reject({responseText: JSON.stringify(reason)});
-    }, deferred.reject);
+    getPromise.then(function (resp) {
+      resp = _.isString(resp) ? JSON.parse(resp) : resp;
+      const row = resp.rows[0];
+      if (row.value === 6) {
+        resolve();
+      }
+      reject(new Error('Values expect 6, got ' + row.value));
+    }, reject);
 
-    return deferred;
+    return promise;
   },
 
   setupView: function () {
-    var doc1 = new Documents.Doc({_id: 'test_doc_10', a: 1}, { database: db });
-    var doc2 = new Documents.Doc({_id: 'test_doc_20', a: 2}, { database: db });
-    var doc3 = new Documents.Doc({_id: 'test_doc_30', a: 3}, { database: db });
+    const doc1 = new Documents.Doc({_id: 'test_doc_10', a: 1}, { database: db });
+    const doc2 = new Documents.Doc({_id: 'test_doc_20', a: 2}, { database: db });
+    const doc3 = new Documents.Doc({_id: 'test_doc_30', a: 3}, { database: db });
 
     viewDoc = new Documents.Doc({
       _id: '_design/view_check',
@@ -113,43 +115,40 @@ Verifyinstall.testProcess = {
       database: db
     });
 
-    return FauxtonAPI.when([doc1.save(), doc2.save(), doc3.save(), viewDoc.save()]);
+    return FauxtonAPI.Promise.all([doc1.save(), doc2.save(), doc3.save(), viewDoc.save()]);
   },
 
   setupReplicate: function () {
-    return $.ajax({
-      url: app.host + '/_replicate',
-      contentType: 'application/json',
-      type: 'POST',
-      dataType: 'json',
-      processData: false,
-      data: JSON.stringify({
-        create_target: true,
-        source: 'verifytestdb',
-        target: 'verifytestdb_replicate'
-      })
+    const body = {
+      create_target: true,
+      source: 'verifytestdb',
+      target: 'verifytestdb_replicate'
+    };
+    return post(
+      app.host + '/_replicate',
+      body
+    ).then(res => {
+      if (res.error) {
+        throw new Error(res.reason || res.error);
+      }
+      return res;
     });
   },
 
   testReplicate: function () {
-    var deferred = FauxtonAPI.Deferred();
-    var promise = dbReplicate.fetch();
+    const promise = new FauxtonAPI.Promise((resolve, reject) => {
+      dbReplicate.fetch().then(() => {
+        const docCount = dbReplicate.get('doc_count');
+        if (docCount === 4) {
+          resolve();
+          return;
+        }
+        const reason = 'Replication Failed, expected 4 docs got ' + docCount;
+        reject(new Error(reason));
+      }, reject);
+    });
 
-    promise.then(function () {
-      var docCount = dbReplicate.get('doc_count');
-      if (docCount === 4) {
-        deferred.resolve();
-        return;
-      }
-
-      var reason = {
-        reason: 'Replication Failed, expected 4 docs got ' + docCount
-      };
-
-      deferred.reject({responseText: JSON.stringify(reason)});
-    }, deferred.reject);
-
-    return deferred;
+    return promise;
   },
 
   removeDBs: function () {
