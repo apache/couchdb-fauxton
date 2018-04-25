5db87795a39afb16bc232b0480e929fce286a9d1
Garren Smith <garren.smith@gmail.com>
2018-04-25 11:55:00 +0200

    fix tests

--- app/addons/replication/__tests__/api.tests.js
+++ app/addons/replication/__tests__/api.tests.js
@@ -548,10 +548,10 @@ describe('Replication API', () => {
 
       it("returns parsedReplicationDocs and ignores all design docs", () => {
         fetchMock.getOnce('/_scheduler/jobs', 404);
-        fetchMock.get('/_replicator/_all_docs?include_docs=true&limit=100', _repDocs);
+        fetchMock.get('/_replicator/_all_docs?limit=6&skip=0&include_docs=true', _repDocs);
         return supportNewApi(true)
-          .then(fetchReplicationDocs)
-          .then(docs => {
+          .then(() => fetchReplicationDocs({docsPerPage: 5, page: 1}))
+          .then(({docs}) => {
             assert.deepEqual(docs.length, 1);
             assert.deepEqual(docs[0]._id, "c94d4839d1897105cb75e1251e0003ea");
           });
@@ -565,11 +565,11 @@ describe('Replication API', () => {
 
       it("returns parsedReplicationDocs", () => {
         fetchMock.getOnce('/_scheduler/jobs', 200);
-        fetchMock.get('/_replicator/_all_docs?include_docs=true&limit=100', _repDocs);
-        fetchMock.get('/_scheduler/docs?include_docs=true', _schedDocs);
+        fetchMock.get('/_replicator/_all_docs?limit=6&skip=0&include_docs=true', _repDocs);
+        fetchMock.get('/_scheduler/docs?limit=6&skip=0&include_docs=true', _schedDocs);
         return supportNewApi(true)
-          .then(fetchReplicationDocs)
-          .then(docs => {
+          .then(() => fetchReplicationDocs({docsPerPage: 5, page: 1}))
+          .then(({docs}) => {
             assert.deepEqual(docs.length, 1);
             assert.deepEqual(docs[0]._id, "c94d4839d1897105cb75e1251e0003ea");
             assert.deepEqual(docs[0].stateTime.toDateString(), (new Date('2017-03-07T14:46:17')).toDateString());
