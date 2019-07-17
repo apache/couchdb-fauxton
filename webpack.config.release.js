414769a680c113312059d407008f7fe9bc961fec
Antonio Maranhao <30349380+Antonio-Maranhao@users.noreply.github.com>
2019-07-17 13:27:48 -0400

    Upgrade to webpack 4 (#1210)
    Use webpack 4 and latest version of the webpack loaders.
    Update webpack configurations:
     - MiniCssExtractPlugin replaced the ExtractTextPlugin
     - Options for UglifyJsPlugin, CommonsChunkPlugin, DefinePlugin and
       LimitChunkCountPlugin were removed since webpack 4 now provides
       the same features as built-in options.

--- webpack.config.release.js
+++ webpack.config.release.js
@@ -9,10 +9,10 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const settings = require('./tasks/helper')
   .init()
   .readSettingsFile()
@@ -20,6 +20,9 @@ const settings = require('./tasks/helper')
   .release;
 
 module.exports = {
+
+  mode: 'production',
+
   // Entry point for static analyzer:
   entry: {
     bundle: ['core-js/fn/array', 'core-js/fn/string/ends-with', 'core-js/fn/string/starts-with',  'core-js/fn/object', 'core-js/fn/symbol', 'core-js/fn/promise', 'regenerator-runtime/runtime', './app/main.js']
@@ -27,36 +30,14 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, '/dist/release/'),
-    filename: 'dashboard.assets/js/[name].[chunkhash].js'
+    filename: 'dashboard.assets/js/[name].[chunkhash].js',
+    chunkFilename: 'dashboard.assets/js/[name].[chunkhash].js'
   },
 
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1}),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production') // This has effect on the react lib size
-      }
-    }),
     // moment doesn't offer a modular API, so manually remove locale
     // see https://github.com/moment/moment/issues/2373
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      sourceMap: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor', // Specify the common bundle's name.
-      minChunks: function (module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest",
-      minChunks: Infinity
-    }),
     new HtmlWebpackPlugin(Object.assign({
       template: settings.src,
       title: 'Project Fauxton',
@@ -64,8 +45,28 @@ module.exports = {
       generationLabel: 'Fauxton Release',
       generationDate: new Date().toISOString()
     }, settings.variables)),
-    new ExtractTextPlugin("dashboard.assets/css/styles.[chunkhash].css"),
+    new MiniCssExtractPlugin({
+      filename: 'dashboard.assets/css/styles.[chunkhash].css',
+      chunkFilename: 'dashboard.assets/css/styles.[chunkhash].css'
+    })
   ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10,
+          enforce: true
+        },
+      }
+    },
+    runtimeChunk: {
+      name: "manifest",
+    }
+  },
 
   resolve: {
     extensions: ['*', '.js', '.jsx'],
@@ -75,7 +76,7 @@ module.exports = {
   },
 
   module: {
-    loaders: [{
+    rules: [{
       test: /\.jsx?$/,
       enforce: "pre",
       use: ['eslint-loader'],
@@ -106,22 +107,25 @@ module.exports = {
     },
     {
       test: /\.less/,
-      use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: [
-          "css-loader",
-          {
-            loader: "less-loader",
-            options: {
-              modifyVars: {
-                largeLogoPath: "'" + settings.variables.largeLogoPath + "'",
-                smallLogoPath: "'" + settings.variables.smallLogoPath + "'"
-              }
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '../../',
+            hmr: false,
+          },
+        },
+        "css-loader",
+        {
+          loader: "less-loader",
+          options: {
+            modifyVars: {
+              largeLogoPath: "'" + settings.variables.largeLogoPath + "'",
+              smallLogoPath: "'" + settings.variables.smallLogoPath + "'"
             }
           }
-        ],
-        publicPath: '../../'
-      }),
+        }
+      ]
     },
     {
       test: /\.css$/, use: [
