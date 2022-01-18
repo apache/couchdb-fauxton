3a75e06f3c4923cac0c018141dc7202282b120a6
Rhys Short <rhyshort@uk.ibm.com>
2022-01-18 17:50:54 +0000

    Replace brace with ace-build and react-ace
    To enable removal of the CSP directive `style-src unsafe-inline`, the
    ace editor version needs to be upgraded to at least 1.4.13 which
    includes the global option `useStrictCSP`. In order to do this
    brace has had to be replaced since it hasn't had an update in 4 years.
    react-ace is chosen as a replacement as it uses ace-builds and wraps
    ace in a react component. The option `useStrictCSP` is enabled,
    with the required style sheets imported, where possible properties
    that can be handled by react-ace are passed directly to `AceEditor`
    as part of the `render()` call, otherwise customizations are left
    to be handled by the codeeditor component.

--- app/addons/components/components/stringeditmodal.js
+++ app/addons/components/components/stringeditmodal.js
@@ -15,11 +15,11 @@ import PropTypes from 'prop-types';
 import React from "react";
 import ReactDOM from "react-dom";
 import {Modal} from "react-bootstrap";
-import ace from "brace";
+import ace from "ace-builds";
 import Helpers from "../../documents/helpers";
-require('brace/mode/javascript');
-require('brace/mode/json');
-require('brace/theme/idle_fingers');
+require('ace-builds/src-min-noconflict/mode-javascript');
+require('ace-builds/src-min-noconflict/mode-json');
+require('ace-builds/src-noconflict/theme-idle_fingers');
 
 // this appears when the cursor is over a string. It shows an icon in the gutter that opens the modal.
 export class StringEditModal extends React.Component {
