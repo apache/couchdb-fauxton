fe8149ccd656384cc62a291a472647d5f669c7f6
Margaret Harrigan <39278650+mharrigan26@users.noreply.github.com>
2022-08-16 13:14:40 -0400

    Use radion buttons in create db panel for choosing the type of db (#1363)

--- app/addons/databases/__tests__/components.test.js
+++ app/addons/databases/__tests__/components.test.js
@@ -1,3 +1,4 @@
+
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
 // the License at
@@ -35,6 +36,13 @@ describe('AddDatabaseWidget', () => {
     el.setState({databaseName: 'my-db'});
     el.instance().onAddDatabase();
     sinon.assert.calledWith(Actions.createNewDatabase, 'my-db');
+  });
+
+  it('creates a non-partitioned database', () => {
+    const el = mount(<Views.AddDatabaseWidget showPartitionedOption={true}/>);
+    el.setState({databaseName: 'my-db', partitionedSelected: false});
+    el.instance().onAddDatabase();
+    sinon.assert.calledWith(Actions.createNewDatabase, 'my-db', false);
   });
 
   it('creates a partitioned database', () => {
