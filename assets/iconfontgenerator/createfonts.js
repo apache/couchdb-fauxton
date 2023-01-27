fd0c92c3e26d09a615bdcdc497e545d3f38b0542
Margaret Harrigan <m.harrigan@ibm.com>
2023-01-27 11:32:34 -0800

    align new icons

--- assets/iconfontgenerator/createfonts.js
+++ assets/iconfontgenerator/createfonts.js
@@ -1,12 +1,15 @@
 const svgtofont = require("svgtofont");
 const path = require("path");
+const fauxtonFontname = "fauxtonicon6";
+const generatedFontsDir = "generated";
 
+// Generate the font files (.ttf, .woff, etc) and LESS file
 svgtofont({
   emptyDist: true, // Clear output directory contents
   src: "../icons", // svg path
-  dist: path.resolve(process.cwd(), "fonts"), // output path
+  dist: path.resolve(process.cwd(), generatedFontsDir), // output path
   styleTemplates: path.resolve(process.cwd(), "styles"), // file templates path (optional)
-  fontName: "fauxtonicon6", // font name
+  fontName: fauxtonFontname, // font name
   css: {
     fileName: 'icons',
     cssPath: '../fonts/'
@@ -15,9 +18,51 @@ svgtofont({
   classNamePrefix: "fonticon",
   svgicons2svgfont: {
     fontHeight: 1000,
-    normalize: true
+    normalize: true,
+    descent:64,
   },
   website: null,
 }).then(() => {
-  console.log('done!');
+  console.log('new fonts generated');
+
+  // The custom template ../styles/icons.less includes the '{{cssToVars}}' so the generator adds the list of icons as variables.
+  // The problem is that {{cssToVars}} only supports SCSS syntax, so we need to convert it to LESS syntax, which comes down 
+  // to replacing '$<var_name>' to '@<var_name>'.
+
+  const fs = require('fs');
+  try {
+    const original = fs.readFileSync('./generated/icons.less', 'utf8');
+    const updated = original.replaceAll("$fonticon-", "@fonticon-");
+    fs.writeFileSync('./generated/icons.less', updated);
+
+    // removing old icons font files
+    const fontsDir = '../fonts';
+    fs.readdirSync(fontsDir).forEach(file => {
+      if (file.startsWith('fauxtonicon')) {
+        const toDelete = fontsDir+'/'+file;
+        console.log('Deleting', toDelete);
+        fs.unlinkSync(toDelete);
+      }
+    });
+
+    // add new icons font files
+    fs.readdirSync(generatedFontsDir).forEach(file => {
+      if (file.startsWith('fauxtonicon')) {
+        const from = generatedFontsDir + '/' + file;
+        const to = fontsDir + '/' + file;
+        console.log('Moving', from, 'to', to);
+        fs.renameSync(from, to);
+      }
+    });
+
+    const iconsLess = '../less/icons.less';
+    if (fs.existsSync(iconsLess)) {
+      console.log('Deleting old icons.less', iconsLess);
+      fs.unlinkSync(iconsLess);
+    }
+    const generatedIconsLess = generatedFontsDir + '/icons.less';
+    fs.renameSync(generatedIconsLess, iconsLess);
+  } catch (err) {
+    console.error(err);
+  }
 });
