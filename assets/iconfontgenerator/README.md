07fcce5a20bc81a367448b3a3e1340f230a6de57
Margaret Harrigan <m.harrigan@ibm.com>
2023-01-31 13:27:45 -0800

    apply patch fixes

--- assets/iconfontgenerator/README.md
+++ assets/iconfontgenerator/README.md
@@ -1,15 +1,22 @@
-### How to generate new fonts
+### How to regenerate Fauxton's icon fonts
 
-1. Add your new font(s) in SVG format to the `assets/icons` folder. Be sure there are no extraneous values, only the svg tag and its contents, otherwise svgtofont won't generate CSS/less files.
+The steps below describe how to use [svgtofont](https://github.com/jaywcjlove/svgtofont) to generate a new set of
+CSS and font files based on an input set of SVG icons.
 
-2. Within the iconfontgenerator folder, run the command `node createfonts.js`. That generates the following:
+**IMPORTANT**: The source SVG files can not contain any comments or metatags (e.g. `<?xml ...?>` , `<!-- -->` or `<!DOCTYPE ... >`) otherwise the `svgtofont` tool will fail.
 
-- font files in [LOCAL_COUCHDB_REPO]/couchdb-fauxton/assets/fonts
-  * `[font name].eot` 
-  * `[font name].svg`
-  * `[font name].symbol.svg`
-  * `[font name].ttf`
-  * `[font name].woff` 
-  * `[font name].woff2` 
 
-3. This will overwrite the previous font files and the new icons will be ready to use. All available icons will be in `icons.less` which can be found in [LOCAL_COUCHDB_REPO]/couchdb-fauxton/assets/less.
+1. Add, remove or replace SVG icons in the [assets/icons](assets/icons) folder.
+
+2. Run `npm install svgtofont --no-save` to install the tool.
+
+3. Edit `assets/iconfontgenerator/createfonts.js` and update the value of `fauxtonFontname` which is the name of the new font files.
+This is needed because the font files are bundled as-is by Webpack, so in order to burst the browser's cache, you need to specify a different name.
+
+4. Then run
+```
+cd assets/iconfontgenerator
+node createfonts.js
+```
+
+The new CSS and font files are generated and copied to the appropriate Fauxton folders.
--- assets/iconfontgenerator/createfonts.js
+++ assets/iconfontgenerator/createfonts.js
@@ -1,6 +1,23 @@
 const svgtofont = require("svgtofont");
 const path = require("path");
-const fauxtonFontname = "fauxtonicon6";
+const fs = require("fs");
+
+if (process.argv.length != 3) {
+  console.error(`Usage: node createfonts.js [font-name]`);
+  console.error(
+    `where [font-name] must follow the format "fauxtonicon[INTEGER]"`
+  );
+  process.exit(1);
+}
+
+if (!process.argv[2].startsWith("fauxtonicon")) {
+  console.error(
+    `Invalid value: ${process.argv[2]}. Font name must follow the format "fauxtonicon[INTEGER]"`
+  );
+  process.exit(1);
+}
+
+const fauxtonFontname = process.argv[2];
 const generatedFontsDir = "generated";
 
 // Generate the font files (.ttf, .woff, etc) and LESS file
@@ -8,61 +25,60 @@ svgtofont({
   emptyDist: true, // Clear output directory contents
   src: "../icons", // svg path
   dist: path.resolve(process.cwd(), generatedFontsDir), // output path
-  styleTemplates: path.resolve(process.cwd(), "styles"), // file templates path (optional)
+  styleTemplates: path.resolve(process.cwd(), "styles"), // file templates path
   fontName: fauxtonFontname, // font name
   css: {
-    fileName: 'icons',
-    cssPath: '../fonts/'
+    fileName: "icons",
+    cssPath: "../fonts/",
   }, // Create CSS files.
   startUnicode: 0xea01, // unicode start number
   classNamePrefix: "fonticon",
   svgicons2svgfont: {
     fontHeight: 1000,
     normalize: true,
-    descent:64,
+    descent: 64,
   },
   website: null,
 }).then(() => {
-  console.log('new fonts generated');
-
-  // The custom template ../styles/icons.less includes the '{{cssToVars}}' so the generator adds the list of icons as variables.
-  // The problem is that {{cssToVars}} only supports SCSS syntax, so we need to convert it to LESS syntax, which comes down 
-  // to replacing '$<var_name>' to '@<var_name>'.
-
-  const fs = require('fs');
+  console.log(`Font ${fauxtonFontname} generated`);
   try {
-    const original = fs.readFileSync('./generated/icons.less', 'utf8');
+    // The custom template ../styles/icons.less includes the '{{cssToVars}}' variable so the generator adds the list of icons as variables.
+    // The problem is that {{cssToVars}} is written in SCSS syntax, so we need to convert it to LESS syntax, which comes down
+    // to replacing '$<var_name>' to '@<var_name>'.
+    const original = fs.readFileSync("./generated/icons.less", "utf8");
     const updated = original.replaceAll("$fonticon-", "@fonticon-");
-    fs.writeFileSync('./generated/icons.less', updated);
+    fs.writeFileSync("./generated/icons.less", updated);
 
-    // removing old icons font files
-    const fontsDir = '../fonts';
-    fs.readdirSync(fontsDir).forEach(file => {
-      if (file.startsWith('fauxtonicon')) {
-        const toDelete = fontsDir+'/'+file;
-        console.log('Deleting', toDelete);
+    // Remove old icons font files
+    const fontsDir = "../fonts";
+    fs.readdirSync(fontsDir).forEach((file) => {
+      if (file.startsWith("fauxtonicon")) {
+        const toDelete = fontsDir + "/" + file;
+        console.log("Deleting", toDelete);
         fs.unlinkSync(toDelete);
       }
     });
 
-    // add new icons font files
-    fs.readdirSync(generatedFontsDir).forEach(file => {
-      if (file.startsWith('fauxtonicon')) {
-        const from = generatedFontsDir + '/' + file;
-        const to = fontsDir + '/' + file;
-        console.log('Moving', from, 'to', to);
+    // Add new icons font files
+    fs.readdirSync(generatedFontsDir).forEach((file) => {
+      if (file.startsWith("fauxtonicon")) {
+        const from = generatedFontsDir + "/" + file;
+        const to = fontsDir + "/" + file;
+        console.log("Moving", from, "to", to);
         fs.renameSync(from, to);
       }
     });
 
-    const iconsLess = '../less/icons.less';
+    // Replace 'assets/less/icons.less' with the newly generated version
+    const iconsLess = "../less/icons.less";
+    console.log(`Replacing ${iconsLess} with the new version`);
     if (fs.existsSync(iconsLess)) {
-      console.log('Deleting old icons.less', iconsLess);
       fs.unlinkSync(iconsLess);
     }
-    const generatedIconsLess = generatedFontsDir + '/icons.less';
+    const generatedIconsLess = generatedFontsDir + "/icons.less";
     fs.renameSync(generatedIconsLess, iconsLess);
   } catch (err) {
     console.error(err);
+    process.exit(1);
   }
 });
