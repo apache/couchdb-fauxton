994a35389b6e266ae8b697e92d210f531e46e2e6
Antonio Maranhao <antonio.maranhao@gmail.com>
2023-02-15 18:55:16 -0500

    Fix: replace pull-left with float-start

--- app/addons/documents/rev-browser/components/controller.js
+++ app/addons/documents/rev-browser/components/controller.js
@@ -45,7 +45,7 @@ export default class DiffyController extends React.Component {
 
     // no conflicts happened for this doc
     if (!theirs || !conflictingRevs.length) {
-      return <div style={{textAlign: 'center', color: '#fff'}}><h2>No conflicts</h2></div>;
+      return <div style={{textAlign: 'center', color: '#ffffff'}}><h2>No conflicts</h2></div>;
     }
 
     return (
