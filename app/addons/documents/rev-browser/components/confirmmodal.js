8cd56bf75aa280d1d9e45d7902fe7e25edaa7d24
Antonio Maranhao <antonio.maranhao@gmail.com>
2025-01-17 13:18:12 -0500

    Fix: misaligned buttons in the doc conflicts confirmation modal

--- app/addons/documents/rev-browser/components/confirmmodal.js
+++ app/addons/documents/rev-browser/components/confirmmodal.js
@@ -52,33 +52,27 @@ export default class ConfirmModal extends React.Component {
           <p>
             Do you want to delete all conflicting revisions for this document?
           </p>
-        </Modal.Body>
-        <Modal.Footer>
-
           <div className='col-12'>
             <Form.Check type="checkbox"
               className='do-not-show-again'
               label="Do not show this warning message again"
               onChange={() => { this.setState({checked: !this.state.checked }); }} />
           </div>
+        </Modal.Body>
+        <Modal.Footer>
 
-          <div className="col-auto">
-            <Button href="#"
-              variant="cf-cancel"
-              className='cancel-link'
-              onClick={this.close}
-              data-bypass="true"
-            >
-            Cancel
-            </Button>
-          </div>
-          <div className="col-auto">
-            <ConfirmButton
-              onClick={this.onDeleteConflicts}
-              text="Delete Revisions"
-              variant="danger" />
-          </div>
-
+          <Button href="#"
+            variant="cf-cancel"
+            className='cancel-link'
+            onClick={this.close}
+            data-bypass="true"
+          >
+          Cancel
+          </Button>
+          <ConfirmButton
+            onClick={this.onDeleteConflicts}
+            text="Delete Revisions"
+            variant="danger" />
         </Modal.Footer>
       </Modal>
     );
